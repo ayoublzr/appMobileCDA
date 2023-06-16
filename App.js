b3f7c593d39e4b2bf434da85c26752d4da2c85ae
[Ayoub] <[ayoublzr93@gmail.com]>
2023-06-16 19:05:14 +0200

    update_code

--- App.js
+++ App.js
@@ -1,33 +1,57 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { View, SafeAreaView, StyleSheet } from 'react-native';
 
 import LoginScreen from "./src/Pages/LoginScreen";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MenuDrawer from "./src/Pages/MenuDrawer";
 import Register from "./src/Pages/Register";
+import ChangePassWord from "./src/Pages/ChangePassWord";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen
-          name="login"
-          component={LoginScreen}
-          options={{ title: "Login" }}
-        />
-        <Stack.Screen
-          name="MenuDrawer"
-          component={MenuDrawer}
-          options={{ title: "MenuDrawer" }}
-        />
-        <Stack.Screen
-          name="register"
-          component={Register}
-          options={{ title: "Register" }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+      {
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen
+              name="login"
+              component={LoginScreen}
+              options={{ title: "Login" }}
+            />
+            <Stack.Screen
+              name="MenuDrawer"
+              component={MenuDrawer}
+              options={{ title: "MenuDrawer" }}
+            />
+            <Stack.Screen
+              name="register"
+              component={Register}
+              options={{ title: "Register" }}
+            />
+            <Stack.Screen
+              name="ChangePassWord"
+              component={ChangePassWord}
+              options={{ title: "ChangePassWord" }}
+            />
+
+          </Stack.Navigator>
+        </NavigationContainer>
+      }
+      </View>
+    </SafeAreaView>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  content: {
+    flex: 1,
+    paddingTop : 42,
+  },
+});
---
+++ src/Pages/DevisScreen.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const DevisScreen = () => {
+    return (
+        <View>
+            <Text style={{ color: 'red' }}>Devis</Text>
+        </View>
+    );
+  };
+
+  export default DevisScreen;
